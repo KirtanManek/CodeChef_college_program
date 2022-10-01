7fbdaa8a42a53b2a227d205a904003c6eb1c489e
KirtanManek <manekkirtan@gmail.com>
2022-10-01 18:06:47 +0530

    Create WhichMixture.java

---
+++ week-4/practice/WhichMixture.java
@@ -0,0 +1,26 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.toList;
+
+/* Name of the class has to be "Main" only if the class is public. */
+public class WhichMixture
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		long n = Long.parseLong(bufferedReader.readLine());
+		for(int i = 0; i < n; i++){
+		    List<Integer> h = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+            
+            if(h.get(0) == 0) System.out.println("Liquid");
+            else if(h.get(1) == 0) System.out.println("Solid");
+            else System.out.println("Solution");
+		}
+	}
+}
