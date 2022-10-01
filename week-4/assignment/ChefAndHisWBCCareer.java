54e6b408886c201a84c6878b5271bb057c8014aa
KirtanManek <manekkirtan@gmail.com>
2022-10-01 14:55:25 +0530

    Create ChefAndHisWBCCareer.java

---
+++ week-4/assignment/ChefAndHisWBCCareer.java
@@ -0,0 +1,23 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.toList;
+
+/* Name of the class has to be "Main" only if the class is public. */
+public class ChefAndHisWBCCareer
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(bufferedReader.readLine());
+		for(int i = 0; i < n; i++){
+		    List<Integer> h = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+            System.out.println(Collections.frequency(h, 1) > Collections.frequency(h, 0) ? "YES" : "NO");
+		}
+	}
+}
