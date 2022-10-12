f6aee2c5ee0a5a9f0439bfe9086f16b63f52f480
KirtanManek <manekkirtan@gmail.com>
2022-10-12 20:54:42 +0530

    Create PawriMeme.java

---
+++ week-7/assignment/PawriMeme.java
@@ -0,0 +1,19 @@
+/* package codechef; // don't place package name! */
+
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+/* Change the class name to "Codechef" and remove public. */
+public class PawriMeme
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(bufferedReader.readLine());
+		for(int i = 0; i < n; i++){
+		    String p = bufferedReader.readLine();
+            p = p.replaceAll("party", "pawri");
+            System.out.println(p);
+		}
+	}
+}
