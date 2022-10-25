47cd484267fca96c4f630393284bd75661b954d5
KirtanManek <manekkirtan@gmail.com>
2022-10-25 11:14:51 +0530

    Create Coldplay.java

---
+++ week-2/practice/Coldplay.java
@@ -0,0 +1,26 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.toList;
+
+/* Name of the class has to be "Main" only if the class is public. */
+/* Change the class name to "Codechef" and remove public. */
+
+public class Coldplay
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		long n = Long.parseLong(bufferedReader.readLine());
+		for(int i = 0; i < n; i++){
+		    List<Double> h = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Double::parseDouble)
+            .collect(toList());
+            
+            System.out.println((int)Math.floor(h.get(0) / h.get(1)));
+		}
+	}
+}
