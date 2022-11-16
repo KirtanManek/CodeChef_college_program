1ee9c5c7e4ee967fa1d6c31b47c0de55d7f7a2cc
KirtanManek <manekkirtan@gmail.com>
2022-11-16 19:28:14 +0530

    Update CopsAndTheThiefDevu.java

--- week-9/practice/CopsAndTheThiefDevu.java
+++ week-9/practice/CopsAndTheThiefDevu.java
@@ -14,22 +14,22 @@ public class CopsAndTheThiefDevu
 		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(bufferedReader.readLine());
 		for(int i = 0; i < n; i++){
-		    List<Integer> info = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
-            .map(Integer::parseInt)
-            .collect(toList());
-            List<Integer> house = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
-            .map(Integer::parseInt)
-            .collect(toList());
+		    	List<Integer> info = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            			.map(Integer::parseInt)
+            			.collect(toList());
+            		List<Integer> house = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            			.map(Integer::parseInt)
+            			.collect(toList());
 			Collections.sort(house);
 			List<Integer> remaining = new ArrayList<>();
 			for(int j = 1; j <= 100; j++) remaining.add(j);
-            int houses = info.get(1) * info.get(2);
-            int min = 0, max = 0;
-            for(int j = 0; j < house.size(); j++){
+            		int houses = info.get(1) * info.get(2);
+            		int min = 0, max = 0;
+            		for(int j = 0; j < house.size(); j++){
 				min = Math.max(house.get(j) - houses, 0);
 				max = Math.min(house.get(j) + houses, 100);
 				for(int k = min; k <= max; k++) remaining.remove(new Integer(k));
-            }
+            		}
 			System.out.println(remaining.size());
 		}
 	}
