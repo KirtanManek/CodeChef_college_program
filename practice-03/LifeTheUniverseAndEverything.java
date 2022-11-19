3e3fb7533f60d551f22768bbf9757b25b9f846e6
KirtanManek <manekkirtan@gmail.com>
2022-11-19 18:59:02 +0530

    Create LifeTheUniverseAndEverything.java

---
+++ practice-03/LifeTheUniverseAndEverything.java
@@ -0,0 +1,21 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.toList;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		while(true){
+		        int p = Integer.parseInt(bufferedReader.readLine());
+                        if(p == 42) break;
+                        else System.out.print(p + " ");
+		}
+	}
+}
